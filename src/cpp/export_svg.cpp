9fe934c33dadf48c429cbf46d3c0056f1aac0fb1
Jan Hadrava <had@kam.mff.cuni.cz>
2015-07-11 13:40:12 +0200

    cpp: bezpečnější svg export

--- src/cpp/export_svg.cpp
+++ src/cpp/export_svg.cpp
@@ -37,20 +37,27 @@ void svg_write(FILE *fd, const class v_image &image) {
 	setlocale(LC_ALL, "C");
 	svg_write_header(fd, image);
 	for (v_line line: image.line) {
+		if (line.segment.empty())
+			continue;
 		auto segment = line.segment.cbegin();
 		fprintf(fd, "    <path\n");
-		fprintf(fd, "       d=\"M %f %f C %f %f", segment->main.x, segment->main.y, segment->control_next.x, segment->control_next.y);
+		fprintf(fd, "       d=\"M %f %f", segment->main.x, segment->main.y);
+
+		v_pt cn = segment->control_next;
+		int count = 1;
+		p width = segment->width;
+		p opacity = segment->opacity;
 		segment++;
-		auto segment2 = segment;
-		segment2++;
-		while (segment2 != line.segment.cend()) {
-			fprintf(fd, " %f %f %f %f C %f %f", segment->control_prev.x, segment->control_prev.y, segment->main.x, segment->main.y, segment->control_next.x, segment->control_next.y);
+		while (segment != line.segment.cend()) {
+			fprintf(fd, " C %f %f %f %f %f %f", cn.x, cn.y, segment->control_prev.x, segment->control_prev.y, segment->main.x, segment->main.y);
+			cn = segment->control_next;
+			count ++;
+			width += segment->width;
+			opacity += segment->opacity;
 			segment++;
-			segment2++;
 		}
-		fprintf(fd, " %f %f %f %f", segment->control_prev.x, segment->control_prev.y, segment->main.x, segment->main.y);
 		fprintf(fd, "\"\n");
-		fprintf(fd, "       style=\"fill:none;stroke:#000000;stroke-width:%fpx;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:%f\" />\n", segment->width, segment->opacity);
+		fprintf(fd, "       style=\"fill:none;stroke:#000000;stroke-width:%fpx;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:%f\" />\n", width/count, opacity/count);
 	}
 	svg_write_footer(fd, image);
 }
