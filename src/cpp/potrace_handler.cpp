f38dcbe9ec4c47a1e656c638241bea761bd5a017
Jan Hadrava <had@kam.mff.cuni.cz>
2015-07-13 00:13:27 +0200

    cpp: ladění potrace_handleru

--- src/cpp/potrace_handler.cpp
+++ src/cpp/potrace_handler.cpp
@@ -10,137 +10,62 @@
 using namespace vect;
 using namespace pnm;
 
-
-/* ---------------------------------------------------------------------- */
-/* auxiliary bitmap functions */
-
-/* macros for writing individual bitmap pixels */
-#define BM_WORDSIZE ((int)sizeof(potrace_word))
-#define BM_WORDBITS (8*BM_WORDSIZE)
-#define BM_HIBIT (((potrace_word)1)<<(BM_WORDBITS-1))
-#define bm_scanline(bm, y) ((bm)->map + (y)*(bm)->dy)
-#define bm_index(bm, x, y) (&bm_scanline(bm, y)[(x)/BM_WORDBITS])
-#define bm_mask(x) (BM_HIBIT >> ((x) & (BM_WORDBITS-1)))
-#define bm_range(x, a) ((int)(x) >= 0 && (int)(x) < (a))
-#define bm_safe(bm, x, y) (bm_range(x, (bm)->w) && bm_range(y, (bm)->h))
-#define BM_USET(bm, x, y) (*bm_index(bm, x, y) |= bm_mask(x))
-#define BM_UCLR(bm, x, y) (*bm_index(bm, x, y) &= ~bm_mask(x))
-#define BM_UPUT(bm, x, y, b) ((b) ? BM_USET(bm, x, y) : BM_UCLR(bm, x, y))
-#define BM_PUT(bm, x, y, b) (bm_safe(bm, x, y) ? BM_UPUT(bm, x, y, b) : 0)
-
-/* return new un-initialized bitmap. NULL with errno on error */
-static potrace_bitmap_t *bm_new(int w, int h) {
-  potrace_bitmap_t *bm;
-  int dy = (w + BM_WORDBITS - 1) / BM_WORDBITS;
-
-  bm = (potrace_bitmap_t *) malloc(sizeof(potrace_bitmap_t));
-  if (!bm) {
-    return NULL;
-  }
-  bm->w = w;
-  bm->h = h;
-  bm->dy = dy;
-  bm->map = (potrace_word *) calloc(h, dy * BM_WORDSIZE);
-  if (!bm->map) {
-    free(bm);
-    return NULL;
-  }
-  return bm;
-}
-
-/* free a bitmap */
-static void bm_free(potrace_bitmap_t *bm) {
-  if (bm != NULL) {
-    free(bm->map);
-  }
-  free(bm);
-}
-
-/* ---------------------------------------------------------------------- */
-/* demo */
-
 v_image vectorize_potrace(const pnm_image &original) {
 	pnm_image image = original;
 	image.convert(PNM_BINARY_PBM);
 
+	potrace_bitmap_t pot_bitmap;
+	pot_bitmap.w = image.width;
+	pot_bitmap.h = image.height;
+	pot_bitmap.dy = ((image.width - 1) / (8*sizeof(potrace_word))) + 1;
+	pot_bitmap.map = new potrace_word[pot_bitmap.h * pot_bitmap.dy];
+
+	for (int i; i < image.height; i++) {
+		for (int j; j < pot_bitmap.dy; j++) {
+			pot_bitmap.map[i*pot_bitmap.dy + j] = 0;
+			for (int x = 0; (x < sizeof(potrace_word)) && (x+j*sizeof(potrace_word) < ((image.width-1)/8+1)); x++) {
+				pot_bitmap.map[i*pot_bitmap.dy + j] |= image.data[i*((image.width-1)/8+1) + j*sizeof(potrace_word) + x] << (8*(sizeof(potrace_word)-x-1));
+			}
+		}
+	}
+
+	potrace_param_t *pot_params = potrace_param_default();
+	if (!pot_params) {
+		fprintf(stderr, "Error allocating parameters: %s\n", strerror(errno)); 
+		throw 1;//TODO
+	}
+
+	potrace_state_t *pot_state = potrace_trace(pot_params, &pot_bitmap);
+	if (!pot_state || pot_state->status != POTRACE_STATUS_OK) {
+		fprintf(stderr, "Error tracing bitmap: %s\n", strerror(errno));
+		throw 1;
+	}
+
+	delete [] pot_bitmap.map;
 	v_image vector(image.width, image.height);
 
-  int x, y, i;
-  potrace_bitmap_t *bm;
-  potrace_param_t *param;
-  potrace_path_t *p;
-  potrace_state_t *st;
-  int n, *tag;
-  potrace_dpoint_t (*c)[3];
+	potrace_path_t *pot_path = pot_state->plist;
+	while (pot_path) {
+		v_line line;
+		line.add_point(v_pt(pot_path->curve.c[pot_path->curve.n - 1][2].x, pot_path->curve.c[pot_path->curve.n - 1][2].y));
+		for (int n = 0; n < pot_path->curve.n; n++) {
+			if (pot_path->curve.tag[n] == POTRACE_CORNER) {
+				line.add_point(v_pt(pot_path->curve.c[n][1].x, pot_path->curve.c[n][1].y));
+				line.add_point(v_pt(pot_path->curve.c[n][2].x, pot_path->curve.c[n][2].y));
+			}
+			else if (pot_path->curve.tag[n] == POTRACE_CURVETO) {
+				line.add_point( \
+						v_pt(pot_path->curve.c[n][0].x, pot_path->curve.c[n][0].y), \
+						v_pt(pot_path->curve.c[n][1].x, pot_path->curve.c[n][1].y), \
+						v_pt(pot_path->curve.c[n][2].x, pot_path->curve.c[n][2].y));
+			}
+		}
+		vector.add_line(line);
+		pot_path = pot_path->next;
+	}
 
-  /* create a bitmap */
-  bm = bm_new(image.width, image.height);
-  if (!bm) {
-    fprintf(stderr, "Error allocating bitmap: %s\n", strerror(errno)); 
-    throw 1;
-  }
+	potrace_state_free(pot_state);
+	potrace_param_free(pot_params);
 
-  /* fill the bitmap with some pattern */
-  for (y=0; y<image.height; y++) {
-    for (x=0; x<image.width; x++) {
-      BM_PUT(bm, x, y, ((x*x + y*y*y) % 10000 < 5000) ? 1 : 0);
-    }
-  }
-
-  /* set tracing parameters, starting from defaults */
-  param = potrace_param_default();
-  if (!param) {
-    fprintf(stderr, "Error allocating parameters: %s\n", strerror(errno)); 
-    throw 1;
-  }
-  param->turdsize = 0;
-
-  /* trace the bitmap */
-  st = potrace_trace(param, bm);
-  if (!st || st->status != POTRACE_STATUS_OK) {
-    fprintf(stderr, "Error tracing bitmap: %s\n", strerror(errno));
-    throw 1;
-  }
-  bm_free(bm);
-  
-  /* output vector data, e.g. as a rudimentary EPS file */
-  printf("%%!PS-Adobe-3.0 EPSF-3.0\n");
-  printf("%%%%BoundingBox: 0 0 %d %d\n", image.width, image.height);
-  printf("gsave\n");
-
-  /* draw each curve */
-  p = st->plist;
-  while (p != NULL) {
-    n = p->curve.n;
-    tag = p->curve.tag;
-    c = p->curve.c;
-    printf("%f %f moveto\n", c[n-1][2].x, c[n-1][2].y);
-    for (i=0; i<n; i++) {
-      switch (tag[i]) {
-      case POTRACE_CORNER:
-	printf("%f %f lineto\n", c[i][1].x, c[i][1].y);
-	printf("%f %f lineto\n", c[i][2].x, c[i][2].y);
-	break;
-      case POTRACE_CURVETO:
-	printf("%f %f %f %f %f %f curveto\n", 
-	       c[i][0].x, c[i][0].y,
-	       c[i][1].x, c[i][1].y,
-	       c[i][2].x, c[i][2].y);
-	break;
-      }
-    }
-    /* at the end of a group of a positive path and its negative
-       children, fill. */
-    if (p->next == NULL || p->next->sign == '+') {
-      printf("0 setgray fill\n");
-    }
-    p = p->next;
-  }
-  printf("grestore\n");
-  printf("%%EOF\n");
-  
-  potrace_state_free(st);
-  potrace_param_free(param);
-
-  return vector;
+	return vector;
 }
